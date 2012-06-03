d9ff676f0c635857ba888c7be0492c9cd6a945e4
sgny9949 <sgny9949@gmail.com>
2012-06-03 10:36:41 +0900

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,10 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
- def create
+  def create
     @user = User.new(params[:user])
     if @user.save
+      sign_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -23,9 +23,11 @@ describe "User pages" do
         fill_in "Password",     with: "foobar"
         fill_in "Confirmation", with: "foobar"
       end
-
-      it "should create a user" do
-        expect { click_button submit }.to change(User, :count).by(1)
+      describe "after saving the user" do
+        it "should create a user" do
+          expect { click_button submit }.to change(User, :count).by(1)
+        end
+        #it { should have_link('Sign out') }      
       end
     end
   end
